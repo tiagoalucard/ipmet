50eba66c304fd5d45362c5579bc262a9b918ee63
Tiago <tiagoalucard@gmail.com>
2018-09-05 01:38:13 -0300

    done

--- src/IPMet.js
+++ src/IPMet.js
@@ -21,18 +21,24 @@ export default class IPMet extends React.Component {
       <View>
         <View style={{height: height-60}}>
             <WebView
+            ref={WEBVIEW_REF => (WebViewRef = WEBVIEW_REF)}
             source={{uri: 'https://www.ipmet.unesp.br/mobile2/ipmet/'}}
             style={{marginTop: 20}}
             />
-        </View>          
-        <View style={{position: 'absolute', height: this.state.heightAds, top: height-60}}>
-          <TouchableHighlight onPress={() => Linking.openURL('https://www.ipmet.unesp.br/mobile2/openlayers/ipmet/radar.php')} style={{position: 'absolute', height: 50, width: 50, bottom: 60, left: 100}}>
+        </View>
+        <TouchableHighlight underlayColor="#00000001" onPress={() => Linking.openURL('https://www.ipmet.unesp.br/mobile2/openlayers/ipmet/radar.php')} 
+          style={{flex: 1, position: 'absolute', height: 55, width: 70, marginTop: height-116, marginLeft: width-283}}>
             <Text></Text>
-          </TouchableHighlight>         
-            <AdMobBanner            
-            bannerSize="fullBanner"
-            adUnitID="ca-app-pub-8859501611942566/5828561238" // Test ID, Replace with your-admob-unit-id
-            onDidFailToReceiveAdWithError={this.erroAds} />
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor="#00000001" onPress={() => WebViewRef.reload()} 
+          style={{flex: 1, position: 'absolute', height: 55, width: 88, marginTop: height-116,  marginLeft: width-88}}>
+            <Text></Text>
+          </TouchableHighlight>          
+        <View style={{height: this.state.heightAds}}>          
+          <AdMobBanner            
+          bannerSize="fullBanner"
+          adUnitID="ca-app-pub-8859501611942566/5828561238" // Test ID, Replace with your-admob-unit-id
+          onDidFailToReceiveAdWithError={this.erroAds} />
         </View>  
       </View>  
     );
